911910683a44dbe4b8d27cf82fc19b42b786f839
ShaoAnYu <657641986@qq.com>
2024-02-07 00:10:37 +0800

    Update txt.js

--- txt.js
+++ txt.js
@@ -2,3 +2,13 @@ jest.mock('react-redux',  () =>  ({
   ...jest.requireActual('react-redux'),
   useDispatch: jest.fn(),
 }));
+
+  // 模拟 action 函数
+  const fetchDataMock = jest.spyOn(actions, 'fetchUserInfo').mockImplementation(() => {
+    return async (dispatch) => {
+      dispatch({ type: 'FETCH_USERINFO' });
+    };
+  });
+
+expect(fetchDataMock).toHaveBeenCalled();
+
