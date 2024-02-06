c6dd72cffbc28c6d42ef6a5ebdca5137f272b980
ShaoAnYu <657641986@qq.com>
2024-02-06 22:58:31 +0800

    Update txt.js

--- txt.js
+++ txt.js
@@ -1,5 +1,4 @@
-Object.entries(data).map((value)=>({"value":value[0],"name":value[1]}))
-
-<div> title </div>
-
-<div>  <span> name </span> <span> value <span>  <div>
+jest.mock('react-redux',  () =>  ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: jest.fn(),
+}));
