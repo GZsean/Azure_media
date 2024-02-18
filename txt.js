81f97ef7c90e35741c79d61062d4410d46d5f56d
ShaoAnYu <657641986@qq.com>
2024-02-18 10:24:42 +0800

    Update txt.js

--- txt.js
+++ txt.js
@@ -1,16 +1,36 @@
-jest.mock('react-redux',  () =>  ({
-  ...jest.requireActual('react-redux'),
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import ProductCard from './ProductCard';
+//定义组件要传的参数，假数据
+const product = {
+  id: 1,
+  url: 'example.jpg',
+  title: 'Example Title',
+  desc: 'Example Description',
+  doc: 'example.pdf',
+  docName: 'example_document.pdf'
+};
+
+// 定义滴测试 function 组件里面用到了 translate t('')
+const t = jest.fn((text) => text);
+
+// 我组件度用左useDispatch 
+jest.mock('react-redux', () => ({
   useDispatch: jest.fn(),
 }));
 
-import * as actions from './action/UserDetail-action';
-
-  // 模拟 action 函数
-  const fetchDataMock = jest.spyOn(actions, 'fetchUserInfo').mockImplementation(() => {
-    return async (dispatch) => {
-      dispatch({ type: 'FETCH_USERINFO' });
-    };
+describe('ProductCard component', () => {
+  it('renders product details correctly', () => {
+    const { getByText } = render(<ProductCard product={product} />);
+    
+    expect(getByText('Example Title')).toBeInTheDocument();
+    expect(getByText('Example Description')).toBeInTheDocument();
+    expect(getByText('ViewMore')).toBeInTheDocument();
   });
 
-expect(fetchDataMock).toHaveBeenCalled();
-
+  it('navigates to correct link on view more click', () => {
+    const { getByText } = render(<ProductCard product={product} />);
+    
+    fireEvent.click(getByText('ViewMore'));
+  });
+});
