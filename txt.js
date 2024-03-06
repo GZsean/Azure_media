8a030d03892be0063647adeb5c7d6eb5512c7d21
ShaoAnYu <657641986@qq.com>
2024-03-06 22:46:49 +0800

    Update txt.js

--- txt.js
+++ txt.js
@@ -2,9 +2,13 @@
   border: none;
   width: 50px !important;
   height: 50px !important;
+  padding: 0 !important;
 }
 .ant-color-picker-color-block {
   width: 50px !important;
   height: 50px !important;
   border: 1px solid #ccc;
+}
+.ant-color-picker-trigger-active {
+  border: none !important;
 }
