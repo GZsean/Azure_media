3d8e5f2d22d332f9c01e76bae965f089cd5987f7
ShaoAnYu <657641986@qq.com>
2024-03-19 22:38:49 +0800

    Update txt.js

--- txt.js
+++ txt.js
@@ -2,44 +2,41 @@ import React from "react";
 import "./index.css";
 import { Col, ColorPicker, Divider, Row, Space, theme } from "antd";
 import { generate, green, presetPalettes, red, cyan } from "@ant-design/colors";
-const genPresets = (presets = presetPalettes) =>
-  Object.entries(presets).map(([label, colors]) => ({
-    colors,
-  }));
-const HorizontalLayoutDemo = () => {
-  const { token } = theme.useToken();
-  const presets = genPresets({
-    color: ["#ff0000", "#00ff00", "#0000ff"],
-  });
-  const customPanelRender = (_, { components: { Picker, Presets } }) => (
-    <div style={{ width: "200px" }}>
-      <div>
-        <Picker />
-      </div>
-      <div>
-        <Presets />
-      </div>
-    </div>
-  );
-  return (
-    <ColorPicker
-      defaultValue={token.colorPrimary}
-      styles={{
-        popupOverlayInner: {
-          width: 200,
-        },
-      }}
-      presets={presets}
-      panelRender={customPanelRender}
-    />
-  );
-};
 
-export default () => (
-  <Space direction="vertical">
-    <Space>
-      <span>Horizontal layout:</span>
-      <HorizontalLayoutDemo />
-    </Space>
-  </Space>
+
+const genPresets = (presets = presetPalettes) => Object.entries(presets).map(([label, colors]) => ({
+    colors,
+}));
+  
+
+
+const presets = genPresets({
+  color: ["#ff0000", "#00ff00", "#0000ff"],
+});
+
+const customPanelRender = (_, { components: { Picker, Presets } }) => (
+  <div style={{ width: "200px" }}>
+    <div>
+      <Picker />
+    </div>
+    <div>
+      <Presets />
+    </div>
+  </div>
+);
+
+
+<ColorPicker
+  defaultValue={token.colorPrimary}
+  styles={{
+    popupOverlayInner: {
+      width: 200,
+    },
+  }}
+  presets={presets}
+  panelRender={customPanelRender}
+/>
+
+
+
 );
