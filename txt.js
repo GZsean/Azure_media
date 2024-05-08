6f392719e4b13f6c9216a079c7c90ffb7e38cb09
ShaoAnYu <657641986@qq.com>
2024-05-08 23:09:37 +0800

    Update txt.js

--- txt.js
+++ txt.js
@@ -1,118 +1,91 @@
-"use client"
+const { createRoot } = ReactDOM;
 
-import { useState, useEffect } from 'react'
-import {Row,Col,Input,Button,Select} from 'antd'
+const {  Table  } = antd;
+// In the fifth row, other columns are merged into first column
+// by setting it's colSpan to be 0
 
-const Conditions = ({value=[{
-    id:'a',
-    a:'',
-    b:'',
-c:'',
-d:'',
-e:'',
-f:'',
-g:'',
-h:'',
-i:''}],onChange}) => {
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    render: (text) => <a>{text}</a>,
+    colSpan: 2,
+  },
+  {
+    title: 'Phone',
+    colSpan: 0,
+    dataIndex: 'phone',
+ 
+  },
+  {
+    title: 'Home phone',
+    colSpan: 4,
+    dataIndex: 'tel',
+    
+  },
+  {
+    title: 'Phone',
+    colSpan: 0,
+    dataIndex: 'phone',
+ 
+  },
+  {
+    title: 'Phone',
+    colSpan: 0,
+    dataIndex: 'phone',
+ 
+  },
+  {
+    title: 'Phone',
+    colSpan: 0,
+    dataIndex: 'phone',
+ 
+  },
+];
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    tel: '0571-22098909',
+    phone: 18889898989,
+    address: 'New York No. 1 Lake Park',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    tel: '0571-22098333',
+    phone: 18889898888,
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    tel: '0575-22098909',
+    phone: 18900010002,
+    address: 'Sydney No. 1 Lake Park',
+  },
+  {
+    key: '4',
+    name: 'Jim Red',
+    age: 18,
+    tel: '0575-22098909',
+    phone: 18900010002,
+    address: 'London No. 2 Lake Park',
+  },
+  {
+    key: '5',
+    name: 'Jake White',
+    age: 18,
+    tel: '0575-22098909',
+    phone: 18900010002,
+    address: 'Dublin No. 2 Lake Park',
+  },
+];
+const App = () => <Table columns={columns} dataSource={data} bordered />;
+const ComponentDemo = App;
 
-    const [conditionList, setConditionList] = useState([...value])
-    const [displayValue, setDisplayValue] =useState('')
-    const handleDelete = (id) => {
-        setConditionList(conditionList.filter((item) => item.id !== id));
-    }
 
-    const handleAdd = (ID) =>{
-        setConditionList([...conditionList,{
-            id:ID,
-            a:'',
-            b:'',
-            c:'',
-            d:'',
-            e:'',
-            f:'',
-            g:'',
-            h:'',
-            i:''}])
-        console.log(conditionList)
-    }
-
-    const handleChange = (ID,key,value) =>{
-
-        const updatedList = [...conditionList];
-
-        const itemToUpdate = updatedList.find(item => item.id === ID);
-        if (itemToUpdate) {
-            itemToUpdate[key] = value?.target?.value ? value.target.value:value;
-        }
-
-        setConditionList(updatedList);
-    }
-
-    useEffect(()=>{
-        setDisplayValue(JSON.stringify(conditionList))
-    },[conditionList])
-
-
-    return (
-        <div className="w-full">
-            {
-                conditionList.map((item)=>{
-                    return(
-                        <Row key={item.id} className="mb-4">
-                            <Col span={6} >
-                                <Select
-                                    defaultValue="Type"
-                                    value={item.a}
-                                    style={{ width: 120 }}
-                                    onChange={(value)=>handleChange(item.id,'a',value)}
-                                    options={[
-                                        { value: 'Type', label: 'Type' },
-                                        { value: 'lucy', label: 'Lucy' },
-                                        { value: 'Yiminghe', label: 'yiminghe' },
-                                        { value: 'disabled', label: 'Disabled', disabled: true },
-                                    ]}
-                                />
-                            </Col>
-                            <Col span={12}>
-                                <Row>
-                                    <Col span={6}>
-                                        <Input onChange={(value)=>handleChange(item.id,'b',value)}  value={item.b} placeholder="Basic usage" />
-                                    </Col>
-                                    <Col span={6}>
-                                        <Input onChange={(value)=>handleChange(item.id,'c',value)} value={item.c} placeholder="Basic usage" />
-                                    </Col>
-                                    <Col span={6}>
-                                        <Input onChange={(value)=>handleChange(item.id,'d',value)} value={item.d} placeholder="Basic usage" />
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col span={6}>
-                                        <Input onChange={(value)=>handleChange(item.id,'e',value)} value={item.e} placeholder="Basic usage" />
-                                    </Col>
-                                    <Col span={6}>
-                                        <Input onChange={(value)=>handleChange(item.id,'f',value)} value={item.f} placeholder="Basic usage" />
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col span={6} value={item.g}><Input onChange={(value)=>handleChange(item.id,'g',value)} value={item.g} placeholder="Basic usage" /></Col>
-                                    <Col span={6} value={item.h}><Input onChange={(value)=>handleChange(item.id,'h',value)} value={item.h} placeholder="Basic usage" /></Col>
-                                    <Col span={6} value={item.i}><Input onChange={(value)=>handleChange(item.id,'i',value)} value={item.i} placeholder="Basic usage" /></Col>
-                                </Row>
-                            </Col>
-                            <Col span={6}>
-                                <Button onClick={()=>handleDelete(item.id)}>Remove</Button>
-                            </Col>
-                        </Row>
-                    )
-                })
-            }
-
-            <div className="mt-3">
-                <Button type="default" onClick={()=>handleAdd(window.crypto.getRandomValues(new Uint32Array(10)).toString())}>add Condition</Button>
-            </div>
-            <div>{displayValue}</div>
-        </div>
-    )
-}
-
-export default Conditions
+createRoot(mountNode).render(<ComponentDemo />);
